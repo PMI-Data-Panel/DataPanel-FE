1b69be9f76f1fd1434cd33c117a23a8fc14b12be
minseeeeo <alstj09162@gmail.com>
2025-09-17 14:33:30 +0900

    add 로그인 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import Layout from './components/Layout/Layout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import 컴포넌트
+import LoginPage from './pages/LoginPage/LoginPage';
 import AttendanceDashboard from './pages/AttendanceDashboard/AttendanceDashboard';
+import Layout from './components/Layout/Layout';
+
+// import 스타일
 import './styles/globals.css';
 
 function App() {
   return (
-    <Layout>
-      <AttendanceDashboard />
-    </Layout>
+    <Router>
+      <Routes>
+
+        { /* 로그인 페이지 */}
+        <Route path="/" element={<LoginPage />} />
+
+        { /* 대시보드 페이지 */ }
+        <Route
+          path="/dashboard"
+          element={
+            <Layout>
+              <AttendanceDashboard />
+            </Layout>
+          }
+        />
+
+      </Routes>
+    </Router>
   );
 }
 
